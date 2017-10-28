90c10171493bffa2f68ea534cf64159d92a5a700
mriksman <mriksman@hotmail.com>
2017-10-29 07:28:54 +0800

    Update binary_switch.js
    Added support for leakSMART valve by checking for state.opened.
    Added config option for outlets and switches. Modified state.consumption (consumption only exists for Quirky Outlinks)

--- src/devices/binary_switch.js
+++ src/devices/binary_switch.js
@@ -1,11 +1,18 @@
 const isFan = (state, device, config) =>
   config.fan_ids.indexOf(device.object_id) !== -1;
 
-const isLightBulb = (state, device, config) =>
-  !isFan(state, device, config) && state.consumption === undefined;
-
 const isOutlet = (state, device, config) =>
-  !isFan(state, device, config) && state.consumption !== undefined;
+  state.consumption !== undefined ||
+  config.outlet_ids.indexOf(device.object_id) !== -1;
+
+const isSwitch = (state, device, config) =>
+  state.opened !== undefined ||
+  config.switch_ids.indexOf(device.object_id) !== -1;
+
+const isLightBulb = (state, device, config) =>
+  !isFan(state, device, config) &&
+  !isOutlet(state, device, config) &&
+  !isSwitch(state, device, config);
 
 export default ({ Characteristic, Service }) => {
   return {
@@ -45,7 +52,26 @@ export default ({ Characteristic, Service }) => {
           },
           {
             characteristic: Characteristic.OutletInUse,
-            get: state => state.consumption > 0.1
+            get: state =>
+              state.consumption ? state.consumption > 0.1 : state.powered
+          }
+        ]
+      },
+      {
+        service: Service.Switch,
+        supported: isSwitch,
+        characteristics: [
+          {
+            characteristic: Characteristic.On,
+            supported: state => state.opened === undefined,
+            get: state => state.powered,
+            set: value => ({ powered: !!value })
+          },
+          {
+            characteristic: Characteristic.On,
+            supported: state => state.opened !== undefined,
+            get: state => state.opened,
+            set: value => ({ opened: !!value })
           }
         ]
       }
